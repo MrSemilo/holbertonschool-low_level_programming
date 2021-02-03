0600ee38f8d398f9e3ad977a89406a5b3bd52eb6
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-02-03 22:55:31 +0000

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  *
  * Return: Always 0 (Success)
  */
-#include <stdio>
+#include <stdio.h>
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
