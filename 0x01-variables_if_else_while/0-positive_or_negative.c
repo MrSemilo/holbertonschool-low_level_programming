708e4a43b38ec702aebd569859320f2513c4823f
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-02-04 23:12:56 +0000

    C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,31 @@
-holaaaa
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if n > 0
+	{
+	prinft("is positive\n");
+	}
+	else if n == 0
+	{
+	printf("is zero\n");
+	}
+	else if n < 0
+	{
+	printf("is negative\n");
+	}
+	/* your code goes there */
+	return (0);
+}
