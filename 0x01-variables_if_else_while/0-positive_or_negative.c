f37b262522befcd9941b370e4bc37fa2ca9de3bd
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-02-04 23:20:59 +0000

    C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if n > 0
 	{
-	prinft("is positive\n");
+	prinft("%d is positive\n");
 	}
 	else if n == 0
 	{
-	printf("is zero\n");
+	printf("%d is zero\n");
 	}
 	else if n < 0
 	{
-	printf("is negative\n");
+	printf("%d is negative\n");
 	}
 	/* your code goes there */
 	return (0);
