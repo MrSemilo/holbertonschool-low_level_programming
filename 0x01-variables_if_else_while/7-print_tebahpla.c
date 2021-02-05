6156addd060aeb17be8d6786c3d2e6ed3c4087df
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-02-05 01:32:15 +0000

    C

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Putchar Z to A
+ * Return: 0 for success
+ */
+int main(void)
+{
+char A = 'a';
+
+for (A = 'z'; A >= 'a'; A--)
+putchar(A);
+
+putchar('\n');
+
+return (0);
+}
