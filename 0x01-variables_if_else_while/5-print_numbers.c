ba466caf427c8bd20ca2c1f6a9f459381c9d50df
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-02-05 01:04:02 +0000

    C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib-h>
+#include <time.h>
+/**
+ * main - Imprimir numeros
+ * Return: 0 for success
+ */
+int main(void)
+{
+char N = '0';
+
+for (N = '0'; N <= '9'; ch++)
+putchar(N);
+putchar('\n');
+return (0);
+}
