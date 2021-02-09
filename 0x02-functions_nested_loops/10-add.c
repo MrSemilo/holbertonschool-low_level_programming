df3bbd9b3aeb7e1d92f0497c7bb228f7cc9c7941
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-02-09 22:32:59 +0000

    C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/*
+* add - sum between
+*
+* @a : number
+*
+* @b : number
+* return: result
+*/
+int add(int a, int b):
+{
+int result
+{
+rsult = a + b:
+return (result);
+}
+}
