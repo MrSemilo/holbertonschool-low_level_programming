c72b9412a2288068d107e2a26e7c677870688172
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-02-09 20:13:23 +0000

    C

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
-/*
-*
-* printf Holberton
-*
-* return 0
-*/
+#include "holberton.h"
+/**
+ * main - Prints Holberton as a message.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("Holberton\n");
+int str[] =[72, 111, 108, 98, 101, 114, 116, 111, 110];
+int coutm, zs;
+
+zs = sizeof(str) / sizeof(int)
+for (coutm = 0; coutm < zs; coutm++)
+{
+_putchar(str[coutm]);
+}
+_putchar('\n');
 return (0);
 }
