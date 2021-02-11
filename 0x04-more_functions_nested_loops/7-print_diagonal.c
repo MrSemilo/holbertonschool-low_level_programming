d68071a05c0de96ace0d04dba55cf42630f014cf
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-02-11 23:53:15 +0000

    C

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_diagonal - prints straight line n times.
+ * @n: times straight line is printed.
+ * Return: no return.
+ */
+void print_diagonal(int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+_putchar(92);
+}
+_putchar('\n');
+}
