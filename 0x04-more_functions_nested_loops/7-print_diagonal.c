881bec59632d4953dee332310594e244f817de84
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-02-12 01:55:52 +0000

    C

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,11 +6,17 @@
  */
 void print_diagonal(int n)
 {
-int i;
+int i, n;
 
 for (i = 0; i < n; i++)
 {
+for (u = 0; u < i; u++)
+{
+_putchar(' ');
+}
 _putchar(92);
+if(i < (n - 1))
+_putchar ('\n');
 }
 _putchar('\n');
 }
