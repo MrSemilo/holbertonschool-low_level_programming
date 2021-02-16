213ba514fc061e96a8c41fb5aa89c57e1a8085ac
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-02-16 03:17:09 +0000

    C

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <holberton.h>
+/**
+ * swap_int - void swap
+ * @a: variable
+ * @b: variable
+ */
+void swap_int(int *a, int *b)
+{
+int rever;
+
+rever = *a;
+*a = *b;
+*b = rever;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strlen - This function 
+ *
+ * @s: this is my 
+ *
+ * Return: the las 
+ */
+
+int _strlen(char *s)
+{
+int a;
+
+for (a = 0; s[a] != '\0' ; a++)
+{
+}
+return (a);
+}
