e4b6c5390c68ff9a266fdb6e62964dbc7c735caf
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-02-17 03:47:07 +0000

    C

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * puts2 - the function 
+ * @str: parameter
+ * 
+ */
+void puts2(char *str)
+{
+	int i;
+	while (str[i] != '\0')	
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
