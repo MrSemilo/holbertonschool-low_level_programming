d8a44e7cf7371c56f4470d29128827e3986f677c
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-02-23 03:55:48 +0000

    c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
 
 /**
- * _memset - This is my function
- * @i: This is my entry and return
+ * _memset - This is my function copy the number the character to S
+ * @s: This is my entry and return
  * @b: This is the character to copy
  * @n: This is the number of Bytes
+ * Return: This is my result and Return S
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i
+	unsigned int i;
 
-	for(i = 0; i < n ; i++)
+	for (i = 0; i < n ; i++)
 	{
 		s[i] = b;
 	}
