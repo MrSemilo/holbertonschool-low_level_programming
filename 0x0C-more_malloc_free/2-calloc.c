f7a17d62591faf3ca30f85ea3b5741b2bec30a1a
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-03-05 02:09:04 +0000

    C

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ *_calloc - allocates memory ofr an array and this memory is set to zero
+ *@nmemb: numbers of elements of the array
+ *@size: size in bytes of each element
+ *
+ *Return: NULL if @nmemb or @size is 0 or if malloc fails allocating memory
+ *        ptr pointer to the memory allocated
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i = 0;
+
+	if (nmemb && size == 0)
+		return (0);
+	ptr = malloc(nmemb * size);
+	if (ptr == 0)
+		return (0);
+	while (i < nmemb * size)
+		ptr[i++] = 0;
+	return (ptr);
+}
