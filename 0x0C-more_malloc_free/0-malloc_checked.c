2363ef43d5ae31c8d962894ceed7a35f5f27c19b
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-03-05 01:01:17 +0000

    C

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "holberton.h" 
+#include <stdlib.h>
+/**
+ * malloc_checked - allocated memory
+ * @b: integer.
+ * Return: ptr or exit 98.
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *i;
+
+	i = malloc(b);
+	if (i == NULL)
+		exit(98);
+	else
+		return (i);
+}
