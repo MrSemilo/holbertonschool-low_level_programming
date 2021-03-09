a2d89b1ee0734b41bb24f7f54a90ca2f78fb64d2
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-03-09 03:56:42 +0000

    C

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Django created you a struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Data structure for holbertn and Django
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif
