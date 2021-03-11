ad2fb3569432426370bc46c32003c79e4a763b07
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-03-11 00:46:01 +0000

    C

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - print array in Hexadecimal and Integer
+ * @array: collection of elements of the type same
+ * @size: size of the array
+ * @action: name function pointer
+ *
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (!action)
+		return;
+	if (!array)
+		return;
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
