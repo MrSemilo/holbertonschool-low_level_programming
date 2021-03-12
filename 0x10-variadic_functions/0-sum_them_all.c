11e315ed1371d9be27bcc69b6e7e81763b0494be
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-03-12 00:14:08 +0000

    .C

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,12 +8,13 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int val = 0;
+	int val;
 	va_list sum;
 	int i;
 
 	if (n == 0)
 		return (0);
+	val = 0;
 	va_start(sum, n);
 	for (i = 0; i < n; i++)
 	{
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,15 +6,4 @@
 
 int sum_them_all(const unsigned int n, ...);
 
-typedef struct printargument
-{
-	char *typeargument;
-	void (*func)();
-} printargument;
-
-void print_c(va_list valist);
-void print_i(va_list valist);
-void print_f(va_list valist);
-void print_s(va_list valist);
-
 #endif
