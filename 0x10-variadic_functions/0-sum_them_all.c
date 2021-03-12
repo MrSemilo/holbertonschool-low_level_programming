f217dd9a91bbed6c10e1f3b526e5fdf5dd0d79e6
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-03-12 03:57:52 +0000

    C

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,14 +1,13 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
  * sum_them_all - returns the sum of all its parameters
  * @n: number of parameters
  * Return: sum
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
-
 	va_list sum;
 
 	int c;
@@ -25,5 +24,6 @@ int sum_them_all(const unsigned int n, ...)
 		c += va_arg(sum, int);
 	}
 	va_end(sum);
+
 	return (c);
 }
