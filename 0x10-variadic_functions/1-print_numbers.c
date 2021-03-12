904a439bbd27665f3626b7b29e59aa51d5918042
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-03-12 00:39:14 +0000

    .C

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,9 @@
 #include "variadic_functions.h"
-
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 unsigned int i;
