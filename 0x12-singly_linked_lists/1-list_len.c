67140f954e023ee7383b6c55a81a7f4d3c3585f9
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-03-19 23:47:01 +0000

    number of elements in a linked

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * list_len - Prints all the elements of a list.
+ *
+ * @h: The number of nodes.
+ *
+ * Return: The number of nodes.
+ */
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	if (!h)
+		return (0);
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
