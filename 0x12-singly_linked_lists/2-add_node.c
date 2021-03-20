85b273d3509d1bbcc828486e4a434cddf43437fd
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-03-20 02:24:48 +0000

    Linked

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include <stdio.h>
 
+/**
+ * add_node - function that adds a new node
+ * @head: linked list data structure header
+ * @str: value of new node
+ * Return: number of nodes
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *nwe;
@@ -18,7 +25,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	while (str[len] != '\0')
 		len++;
-        
+
 	nwe->str = strdup(str);
 	nwe->len = len;
 	nwe->next = *head;
