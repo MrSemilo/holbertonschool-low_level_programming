9f21c667e256ccca44ab4a5200b8ddbd1504ba71
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-03-23 19:33:24 +0000

    linked

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint- delet the head node of a listint_t list.
+ * @head: pointer to pointer the head pointer.
+ * Return: the data of the head node.
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *nwe;
+	int i;
+
+	i = (*head)->i;
+	if (*head == NULL)
+		return (0);
+
+	nwe = (*head)->next;
+	free(*head);
+	*head = nwe;
+	return (i);
+}
