1273a67259c1231a42d66d1fb39e1010cf7db76e
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-03-26 04:11:58 +0000

    bit

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -6,8 +6,7 @@
 #include <unistd.h>
 #include <math.h>
 
-int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-int _putchar(char c);
+void print_binary(unsigned long int n);
 
 #endif
