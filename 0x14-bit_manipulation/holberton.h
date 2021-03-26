288a948ae529c0945fdbe6d22c961e842173e5aa
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-03-26 03:35:39 +0000

    bit

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,6 +1,11 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+
 unsigned int binary_to_uint(const char *b);
 
 #endif
