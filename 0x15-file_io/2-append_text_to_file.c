11a53cf86eee76f0e031f0854d0e3f39cf8898c7
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-03-30 04:58:00 +0000

    File on C

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,32 +1,32 @@
 #include "holberton.h"
 
 /**
- * create_file -function that creates a file.
+ * append_text_to_file - function that appends text at the end of a file.
  * @filename : filename
  * @text_content : text
- * Return: 1 on success, -1 failed
+ * Return: 1 on success and -1 failed
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-int file = 0, len = 0;
+	int file = 0, len = 0;
 
-if (!filename)
-return (-1);
+	if (!filename)
+		return (-1);
 
-if (text_content)
-{
-while (text_content[len])
-{
-len++;
-}
-}
+	if (text_content)
+	{
+		while (text_content[len])
+		{
+			len++;
+		}
+	}
 
-file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	file = open(filename, O_WRONLY | O_APPEND);
 
-if (file < 0)
-return (-1);
+	if (file < 0)
+		return (-1);
 
-write(file, text_content, len);
+	write(file, text_content, len);
 
-return (1);
+	return (1);
 }
