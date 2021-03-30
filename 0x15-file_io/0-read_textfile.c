8d2d006a54bbe5f3e552663419d3d3e55611db3d
samilo-del <https://github.com/samilo-del/holberton-system_engineering-devops.git>
2021-03-30 04:26:59 +0000

    File on C

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - function that reads a text file and prints.
+ * @filename : file
+ * @letters : is the number of letters
+ * Return: the actual number of letters, read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t i = 0, j = 0; 
+int let; 
+char *buf; 
+
+buf = malloc(letters);
+if (buf == NULL)
+return (0);
+
+if (filename == NULL)
+return (0);
+let = open(filename, O_RDONLY);
+
+if (let == -1)
+return (0);
+
+i = read(let, buffer, letters);
+
+if (i == -1)
+return (0);
+
+close(let); 
+j = write(STDOUT_FILENO, buf, i);
+if (j == -1)
+return (0);
+
+return (j);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,3 @@
+0x15. C - File I/O
+ Foundations - Low-level programming & Algorithm  Linux and Unix system programming0x15. C - File I/O
+ Foundations - Low-level programming & Algorithm  Linux and Unix system programming
