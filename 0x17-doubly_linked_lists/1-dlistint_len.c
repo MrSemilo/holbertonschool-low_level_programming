33ec1a5f16b57b7bfbeaaf248b4080f1ed41d660
samilo-del <77862328+samilo-del@users.noreply.github.com>
2021-05-02 15:37:17 -0500

    Create 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - Prototype.
+ * @h: head.
+ * Return: count.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+int count = 0;
+
+if (h == NULL)
+return (0);
+while (h)
+{
+h = h->next;
+count++;
+}
+return (count);
+}
