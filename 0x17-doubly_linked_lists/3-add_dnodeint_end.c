7022f9c86644ce761dfee11989efe687ce88568c
samilo-del <77862328+samilo-del@users.noreply.github.com>
2021-05-02 21:48:12 -0500

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * add_dnodeint_end - Prototype.
  * @head: head.
@@ -8,31 +7,33 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-        dlistint_t *_node, *new_node;
+dlistint_t *_node, *new_node;
 
-        if (head == NULL)
-                return (NULL);
-  
-        new_node = malloc(sizeof(dlistint_t));
-        if (new_node == NULL)
-                return (NULL);
-        new_node->n = n;
-        new_node->next = NULL
-        _node = *head;
+if (head == NULL)
+return (NULL);
 
-        if (*head == NULL)
-        {
-                *head = new_node;
-                new_node->prev = NULL;
-                return (new_node);
-        }
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+new_node->next = NULL;
 
-        while (_node->next != NULL)
-        {
-                _node = _node->next;
-        }
-        _node->next = new_node;
-        new_node->prev = _node;
+_node = *head;
 
-        return (new_node);
+if (*head == NULL)
+{
+*head = new_node;
+new_node->prev = NULL;
+return (new_node);
+}
+
+while (_node->next != NULL)
+{
+_node = _node->next;
+}
+
+_node->next = new_node;
+new_node->prev = _node;
+
+return (new_node);
 }
